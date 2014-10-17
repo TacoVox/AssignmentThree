42ec575494af86538f6690e814d50ffcfeded0dc
TacoVox <jonas.kahler@icloud.com>
2014-10-17 16:25:31 +0200

    Changed the speed changing method

--- Listeners/SetSpeedListener.java
+++ Listeners/SetSpeedListener.java
@@ -7,29 +7,24 @@ class SetSpeedListener implements ActionListener
     //Private attribute speed -> this saves the speed which will be overgiven to the robot
     private double speed;
 
-    //Private attribute rr -> this saves a RandomRobot which will be used to set his speed
-    private RandomRobot rr;
-
     //Private attribute rw -> this saves an object of type RobotWars -> allows us to acces the restart function
+    private RobotWars rw;
 
     //Constructor --> gets a object with the type of the main class
-    public SetSpeedListener(RobotWars rw, RandomRobot rr, double speed)
+    public SetSpeedListener(RobotWars rw, double speed)
     {
-	//Assign the rr to the private attribute of our class
-	this.rr = rr;
-	
 	//Assign the speed to the private attribute of this class
 	this.speed = speed;
+
+	//Assign the rw to the private attribute of this class
+	this.rw = rw;
     }
 
     //Method which gets executed if the button/menu item is clicked
     public void actionPerformed(ActionEvent e)
     {
-	//Restart the game
-	rw.restart();
-
-	//Set the speed of the robot
-	rr.setSpeed(speed);
+	//Restart the game set the speed
+	rw.restart(speed);
 
 	//Start the move
 	rw.gameOn();
