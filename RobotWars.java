006ea174ca8cb142b556740d0e935e224201bdb0
vryashkov <ivo.vryashkov@gmail.com>
2014-10-17 16:34:38 +0200

    main class + random robot

--- RobotWars.java
+++ RobotWars.java
@@ -1,24 +1,30 @@
 
 
+// import galleries
 import becker.robots.*;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 
 import java.awt.*;
 
+
+
 public class RobotWars  
 {
-	private JFrame gameFrame;
-	private JPanel gamePanel;
-	private JButton prButton;
-	private City gameCity;
-	private JPanel controlPanel;
-	private JMenuBar menuBar;
+	private JFrame gameFrame;		// main frame for the game
+	private JPanel gamePanel;		// main panel to hold buttons and city view
+	private JButton prButton;		// pause/resume button
+	private City gameCity;			// the city
+	private JPanel controlPanel;	// panel for the control buttons
+	private JMenuBar menuBar;		// the menu bar
 	
-	private final int size = 11;
+	private int size = 11;
+	private double currentSpeed = 2.0;
 	
-	
+	/**
+	 *  A constructor-creates a new frame with a menu bar,
+	 *  controls and a city view.
+	 */
 	public RobotWars()
 	{
 		gameFrame = new JFrame("Robot Wars");
@@ -27,7 +33,7 @@ public class RobotWars
 		createMenuBar();
 		createMainPanel();
 		createControlPanel();
-		createCity(11, 11);
+		createCity(size, size, currentSpeed);
 	}
 	
 	
@@ -40,8 +46,6 @@ public class RobotWars
 		gamePanel = new JPanel(null);
 		gamePanel.setLayout(new BorderLayout());
 		
-		gameFrame.add(gamePanel);
-		
 		gameFrame.setContentPane(gamePanel);
 		
 		gameFrame.pack();
@@ -50,7 +54,7 @@ public class RobotWars
 	
 	/**
 	 *  A method to create the control panel and the 
-	 *  buttons in it.
+	 *  buttons in it. Listeners are assigned to buttons. 
 	 */
 	private void createControlPanel()
 	{
@@ -103,7 +107,10 @@ public class RobotWars
 	
 	
 	/**
-	 * 
+	 *  A method that creates the menu bar and menus.
+	 *  In each menu there are menu items or radio buttons.
+	 *  Action listeners are also assigned to the different 
+	 *  components.
 	 */
 	private void createMenuBar()
 	{
@@ -112,8 +119,10 @@ public class RobotWars
 		JMenu actions = new JMenu("Actions");
 		
 		JMenuItem pause = new JMenuItem("Pause");
+		pause.addActionListener(new PauseResumeListener(this));
 		actions.add(pause);
 		JMenuItem resume = new JMenuItem("Resume");
+		resume.addActionListener(new PauseResumeListener(this));
 		actions.add(resume);
 		
 		menuBar.add(actions);
@@ -121,8 +130,12 @@ public class RobotWars
 		JMenu settings = new JMenu("Settings");
 		
 		JRadioButton easy = new JRadioButton("Easy");
+		easy.addActionListener(new SetSpeedListener(this, 0.5));
 		JRadioButton normal = new JRadioButton("Normal");
+		normal.addActionListener(new SetSpeedListener(this, 1.0));
+		normal.setSelected(true);
 		JRadioButton hard = new JRadioButton("Hard");
+		hard.addActionListener(new SetSpeedListener(this, 2.0));
 		
 		ButtonGroup group = new ButtonGroup();
 		group.add(easy);
@@ -139,13 +152,28 @@ public class RobotWars
 	}
 	
 	
+	/**
+	 *  A method to create the walls in the city.
+	 */
+	private void createWalls()
+	{
+		for (int i=0; i<size; i++)
+		{
+			new Wall(gameCity, 0, i, Direction.NORTH);
+			new Wall(gameCity, size-1, i, Direction.SOUTH);
+			new Wall(gameCity, i, 0, Direction.WEST);
+			new Wall(gameCity, i, size-1, Direction.EAST);
+		}
+	}
+	
+	
 	/** 
 	 * A method to create the city and the city view.
 	 * 
 	 * @param str	The number of visible streets.
 	 * @param ave	The number of visible avenues.
 	 */
-	private void createCity(int str, int ave)
+	private void createCity(int str, int ave, double speed)
 	{
 		City.showFrame(false);
 		
@@ -159,28 +187,71 @@ public class RobotWars
 		
 		gameFrame.pack();
 		
+		prButton = uic.getStartStopButton();
+		
+		createWalls();
+		
+		RandomRobot karel = new RandomRobot(gameCity, 2, 2, Direction.EAST);
+		
+		karel.setSpeed(speed);
+		
+		Thread karelThread = new Thread(karel);
+		karelThread.start();
+		
 	}
 	
 	
+	/**
+	 *  A method that restarts the game. 
+	 *  First the main panel is removed and then 
+	 *  new main panel, control panel and city is created.
+	 */
 	public void restart()
 	{
 		gameFrame.remove(gamePanel);
 		
 		createMainPanel();
 		createControlPanel();
-		createCity(size, size);
+		createMenuBar();
+		createCity(size, size, currentSpeed);
 	}
 	
+	
+	/**
+	 *  A method for pausing and resuming the game.
+	 */
+	public void pauseResume()
+	{
+		prButton.doClick();
+	}
+	
+	
+	/**
+	 *  A method to start the game with
+	 *  the robots moving.
+	 */
+	public void gameOn()
+	{
+		prButton.doClick();
+	}
 
+	
+	/**
+	 * 
+	 */
+	public void setSpeed(double speed)
+	{
+		currentSpeed = speed;
+	}
+	
+	
 	public static void main(String[] args) 
 	{
 		
-		
 		RobotWars game = new RobotWars();
+		game.gameOn();
 		
-		
-		
-
 	} // end main()
 
+	
 } // end RobotWars
