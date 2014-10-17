d383033814a929d139c52052714890445984d2a1
TacoVox <jonas.kahler@icloud.com>
2014-10-17 17:20:18 +0200

    Changed folder hierarchie

---
+++ listeners/PauseResumeListener.java
@@ -0,0 +1,24 @@
+//Set package name
+package listeners;
+
+//Import necessary libraries
+import java.awt.event.*;
+
+//A listener class for performing pause and resume
+class PauseResumeListener implements ActionListener
+{
+    //Private Attribute rw
+    private RobotWars rw;
+    //Constructor --> gets a object with the type of the main class
+    public PauseResumeListener(RobotWars rw)
+    {
+	//Assign the rw to the private Attribute of our class
+	this.rw = rw;
+    }
+    //Method which gets executed if the button/menu item is clicked
+    public void actionPerformed(ActionEvent e)
+    {
+	//Call the pause and resume function of the rw obj.
+	rw.pauseResume();
+    }
+}
---
+++ listeners/SetSpeedListener.java
@@ -0,0 +1,32 @@
+//Set package name
+package listeners;
+
+//Import necessary libraries
+import java.awt.event.*;
+
+//A listener class for performing pause and resume
+class SetSpeedListener implements ActionListener
+{
+    //Private attribute speed -> this saves the speed which will be overgiven to the robot
+    private double speed;
+    //Private attribute rw -> this saves an object of type RobotWars -> allows us to acces the restart function
+    private RobotWars rw;
+    //Constructor --> gets a object with the type of the main class
+    public SetSpeedListener(RobotWars rw, double speed)
+    {
+	//Assign the speed to the private attribute of this class
+	this.speed = speed;
+	//Assign the rw to the private attribute of this class
+	this.rw = rw;
+    }
+    //Method which gets executed if the button/menu item is clicked
+    public void actionPerformed(ActionEvent e)
+    {
+	//Set a new speed
+	rw.setSpeed(speed);
+	//Restart the game set the speed
+	rw.restart();
+	//Start the move
+	rw.gameOn();
+    }
+}
