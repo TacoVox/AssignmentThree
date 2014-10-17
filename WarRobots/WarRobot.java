69a7cbf9ede39510c6f3f05d17916667e9c26411
TacoVox <jonas.kahler@icloud.com>
2014-10-17 16:00:22 +0200

    Fixed bug

---
+++ WarRobots/WarRobot.java
@@ -0,0 +1,20 @@
+//Import Becker's robot library
+import becker.robots.*;
+
+//A abstract class which allows us to save the implements Runnable later
+public abstract class WarRobot extends RobotSE implements Runnable
+{
+    //Constructor which calls the super constructor
+    public WarRobot(City city, int street, int avenue, Direction dir)
+    {
+	//Call the super constr.
+	super(city, street, avenue, dir);
+    }
+    
+    //The runnable method
+    public void run()
+    {
+	//Override this function
+    }
+    
+}
