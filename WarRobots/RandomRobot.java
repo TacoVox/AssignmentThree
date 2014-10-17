15eb9d2ea6b712234000c3a9ac19fbf700ff7333
TacoVox <jonas.kahler@icloud.com>
2014-10-17 15:47:55 +0200

    Added randMove() method

--- WarRobots/RandomRobot.java
+++ WarRobots/RandomRobot.java
@@ -12,24 +12,50 @@ class RandomRobot extends WarRobot
     {
 	//Call the super constructor
 	super(city, street, avenue, dir);
-    }
+    }//End RandomRobot
 
     //Method which is called when the thread starts
     public void run()
     {
 	//Start the random move around the city
 	randMove();
-    }
+    }//End run()
 
     //Method which starts a random move around the city
     public void randMove()
     {
-	int turns = randomInt(4);
-	
-	switch(turns):
+	int turns;
+
+	while(true)
 	{
-	    case 0:
+		turns = randomInt(4);
 		
-	}
-    }
-}
+		switch(turns):
+		{
+		case 0:
+		    break;
+		case 1:
+		    turnLeft();
+		    break;
+		case 2:
+		    turnAround();
+		    break;
+		case 3:
+		    turnRight();
+		    break;
+		default:
+		    System.err.println("A terrible error has happened - please contact the godlike developer team!");
+		    break;
+		}//End Switch
+		
+		while(!frontIsClear())
+		{
+		    turnLeft();
+		}
+		
+		move();
+	}//End while
+
+    }//End randMove
+
+}//EndClass
