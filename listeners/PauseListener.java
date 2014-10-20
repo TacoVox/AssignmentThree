b3b83e41fc44a039748cbfa4b2d218c6f2bf4027
TacoVox <jonas.kahler@icloud.com>
2014-10-20 16:19:15 +0200

    Change file structure

---
+++ listeners/PauseListener.java
@@ -0,0 +1,26 @@
+//Set package name
+package listeners;
+
+//Import necessary libraries
+import java.awt.event.*;
+import warrobots.*;
+import robotwars.*;
+
+//A listener class for performing pause and resume
+public class PauseListener implements ActionListener
+{
+    //Private Attribute rw
+    private RobotWars rw;
+    //Constructor --> gets a object with the type of the main class
+    public PauseListener(RobotWars rw)
+    {
+	//Assign the rw to the private Attribute of our class
+	this.rw = rw;
+    }
+    //Method which gets executed if the button/menu item is clicked
+    public void actionPerformed(ActionEvent e)
+    {
+	//Call the pause and resume function of the rw obj.
+	rw.pause();
+    }
+}
