7bb5b509676eafccf16a3c4cd4e6282fa385afd3
TacoVox <jonas.kahler@icloud.com>
2014-10-20 16:39:17 +0200

    Added comments

--- warrobots/RandomRobot.java
+++ warrobots/RandomRobot.java
@@ -7,7 +7,7 @@ import becker.robots.*;
 //Import Cezar's random library
 import static dit948.Random.*;
 
-//A class which creates objects which are "robot that move randomly around the map"
+//A class which creates objects which are "a robot which moves randomly around the map"
 public class RandomRobot extends WarRobot
 {
     //Constructor
@@ -16,41 +16,67 @@ public class RandomRobot extends WarRobot
 	//Call the super constructor
 	super(city, street, avenue, dir);
     }//End RandomRobot
+
     //Method which is called when the thread starts
     public void run()
     {
 	//Start the random move around the city
 	randMove();
     }//End run()
+
     //Method which starts a random move around the city
     public void randMove()
     {
+	//Integer which will get assigned later on with random numbers
 	int turns;
+	
+	//A infinite loop
 	while(true)
 	    {
+		//Assign an integer between 0 and 3 to the turns
 		turns = randomInt(4);
+
+		//A switch depending on the turns
 		switch(turns)
 		    {
+		    
+		    //In case turns is a 0
 		    case 0:
+			//Do nothing - just break
 			break;
+		    //In case turns is a 1
 		    case 1:
+			//Turn one time left and then break
 			turnLeft();
 			break;
+		    //In case turns is a 2
 		    case 2:
+			//Turn one time around - then break
 			turnAround();
 			break;
+		    //In case turns is a 3
 		    case 3:
+			//Turn one time right - then break
 			turnRight();
 			break;
+		    //If something completly different is in turns
 		    default:
+			//Print an error string on the error output stream and break
 			System.err.println("A terrible error has happened - please contact the godlike developer team!");
 			break;
 		    }//End Switch
+
+		//If the front of the robot isn't clear (a wall is there)
 		while(!frontIsClear())
-		    {
-			turnLeft();
-		    }
+		{
+		    //Turn left
+		    turnLeft();
+		}
+		
+		//Move one step forward
 		move();
 	    }//End while
+
     }//End randMove
+
 }//EndClass
