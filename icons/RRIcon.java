206e2d53784b694c78c476f0b22d044cb065182b
TacoVox <jonas.kahler@icloud.com>
2014-10-21 16:45:53 +0200

    Changed the fancy looking gifs

--- icons/RRIcon.java
+++ icons/RRIcon.java
@@ -6,6 +6,9 @@ import java.awt.Graphics;
 import java.awt.image.*;
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.io.IOException;
+
+import static dit948.Random.*;
 
 public class RRIcon extends Icon
 {
@@ -21,8 +24,26 @@ public class RRIcon extends Icon
     
     public BufferedImage getRandImage()
     {
-	BufferedImage img = ImageIO.read(new File("red.gif"));
+	BufferedImage img = null;
 	
+	int rand = randomInt(3);
+	
+	String[] paths = new String[3];
+
+	paths[0] = "./icons/data/red.gif";
+	paths[1] = "./icons/data/blue.gif";
+	paths[2] = "./icons/data/orange.gif";
+	
+	try
+        {
+	    img = ImageIO.read(new File(paths[rand]));
+	}
+	catch (IOException e)
+	{
+	    System.err.println("Could not load icon!");
+	    System.err.println(e);    
+	}
+    
 	return img;
     }
 }
