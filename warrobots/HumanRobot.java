e6c737d8d58584cb0e8cf6552b2fb75d086d1697
salkin91 <Niklas@dhcp-077049.eduroam.chalmers.se>
2014-10-21 16:06:26 +0200

    comments

--- warrobots/HumanRobot.java
+++ warrobots/HumanRobot.java
@@ -47,54 +47,55 @@ public class HumanRobot extends WarRobot
 				move();
 		}
 	}
-	
+	//A setDirection method to set the direction of the robot
 	public void setDirection(Direction dir)
 	{
+		//Assigning currentDir to dir
 		currentDir = dir;
 	}
-	
-	private void goNorth()
+	//
+	private void goNorth() //Go north method to tell the robot how to turn
 	{
-		if(isFacingNorth())	
-			System.err.println("already facing north");
-		else if(isFacingEast())
-			turnLeft();
-		else if(isFacingSouth())
-			turnAround();
+		if(isFacingNorth())	//If facing north, continue
+			System.err.println("already facing north"); //An error message if robot facing north
+		else if(isFacingEast()) //If the robot is facing east
+			turnLeft(); //Turn one time left
+		else if(isFacingSouth()) //If facing south
+			turnAround(); //Turn around (two left turns)
 		else 
-			turnRight();
+			turnRight(); // If facing west turn right (90° left)
 	}
-	private void goEast()
+	private void goEast() //Go east method to tell the robot how to turn
 	{
-		if(isFacingEast())
-			System.err.println("already facing east");
-		else if(isFacingSouth())
-			turnLeft();
-		else if(isFacingWest())
-			turnAround();
+		if(isFacingEast()) //If facing east, continue
+			System.err.println("already facing east"); //An error message if robot facing east
+		else if(isFacingSouth()) //If facing south
+			turnLeft(); //Turn one time left
+		else if(isFacingWest()) //If facing west
+			turnAround(); //Turn around (two left turns)
 		else
-			turnRight();
+			turnRight(); //If facing north turn right (90° left)
 	}
-	private void goSouth()
+	private void goSouth() //Go south method to tell the robot how to turn
 	{
-		if(isFacingSouth())
-			System.err.println("already facing south");
-		else if(isFacingWest)
-			turnLeft();
-		else if(isFacingNorth)
-			turnAround();
+		if(isFacingSouth()) //If facing south, continue
+			System.err.println("already facing south"); //An error message if robot facing south
+		else if(isFacingWest) //If facing west
+			turnLeft(); //Turn one time left
+		else if(isFacingNorth) //If facing north
+			turnAround(); //Turn around (two left turns)
 		else 
-			turnRight();
+			turnRight(); //If facing east turn right (90° left)
 	}
-	private void goWest()
+	private void goWest() //Go west method to tell the robot how to turn
 	{
-		if(isFacingWest())
-			System.err.println("already facing west");
-		else if(isFacingNorth())
-			turnLeft();
-		else if(isFacingEast())
-			turnAround();
+		if(isFacingWest()) //If facing west, continue
+			System.err.println("already facing west"); //An error message if robot facing west
+		else if(isFacingNorth()) //If facing north
+			turnLeft(); //Turn left one time
+		else if(isFacingEast()) //If facing east
+			turnAround(); //Turn around (two left turns)
 		else
-			turnRight();
+			turnRight(); //If facing south turn right (90° left)
 	}
 }
