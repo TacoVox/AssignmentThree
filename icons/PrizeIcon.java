16ccf2fb84ee5d2c03e1038984c08f3934ef0946
TacoVox <jonas.kahler@icloud.com>
2014-10-22 16:59:51 +0200

    Changed PrizeIcon formatting

--- icons/PrizeIcon.java
+++ icons/PrizeIcon.java
@@ -13,29 +13,29 @@ import becker.robots.icons.*;
 public class PrizeIcon extends Icon
 {
 	
-	BufferedImage img;
-	
-	public PrizeIcon()
-	{      
-		super();
+    BufferedImage img;
+    
+    public PrizeIcon()
+    {      
+	super();
 
-		try 
-		{
-			img = ImageIO.read(new File("./icons/data/prize.gif"));
-	    	}
-		catch (IOException e) 
-	    	{
-			System.err.println("Error while loading the picture!");
-	    	}
-		setSize(0.4);
-	}
-	
-
-	
-	@Override
-	public void paintIcon(Graphics g)
+	try 
 	{
-		g.drawImage(img, 0, 0, null);
+	    img = ImageIO.read(new File("./icons/data/prize.gif"));
 	}
+	catch (IOException e) 
+	{
+	    System.err.println("Error while loading the picture!");
+	}
+		setSize(0.4);
+    }
+	
+
+	
+    @Override
+    public void paintIcon(Graphics g)
+    {
+	g.drawImage(img, 0, 0, null);
+    }
 	
 }
